c72df60dab8b4fd1ce8640b69744a472bdb04937
eliasn2003 <110858094+EliasN2003@users.noreply.github.com>
2022-09-04 19:25:50 +0300

    Finished last question
    Created the last question interface and moved some comments to better places.

--- Interactive quiz GUI.py
+++ Interactive quiz GUI.py
@@ -5,7 +5,7 @@ from tkinter import *
 
 root = Tk()
 root.title("Hollywood actor quiz")
-root.iconbitmap("images/mate.ico")
+root.iconbitmap("images/icon.ico")
 root.configure(bg="#600AEF")
 root.state('zoomed')
 
@@ -81,7 +81,8 @@ images = [
 
 ]
 
-questions = [       # The questions for the quiz
+# The questions for the quiz
+questions = [
     "",
     "Pick a job you like:",
     "What is your favorite hobby?",
@@ -93,7 +94,8 @@ questions = [       # The questions for the quiz
     "Pick a city you would live in:",
 ]
 
-answers = [         # The answers for the quiz
+# The answers for the quiz
+answers = [
     "", "", "", "", "", "",
     "Diver", "Actor", "Salesman", "Musician", "Footballer", "Teacher",
     "Rock climbing", "Skydiving", "Knitting", "Dancing", "Watching movies", "Shopping",
@@ -119,8 +121,15 @@ x = 0       # Used to automate the printing process of the questions
 y = 0       # Used to automate the printing process of the answers of each question
 
 
+def results():
+    return
+
+
 # The last question that doesn't have buttons under the images
 def last_question():
+    # Clears the main window to print the next question
+    for widget in root.winfo_children():
+        widget.grid_forget()
     title_spacer = Label(root, bg="#600AEF")
     title_spacer.grid(row=0, column=0, columnspan=3)
     question = Label(root, text="Pick a house you would live in:", bg="#600AEF", fg="#FFFF00",
@@ -140,10 +149,10 @@ def last_question():
     Radiobutton(root, image=last_question_images[1], variable=answer, value="female",
                 selectcolor="#4507AB", bg="#600AEF", fg="#FFFF00", activebackground="#FFFF00",
                 activeforeground="#600AEF", indicatoron=False, borderwidth=4) \
-        .grid(row=4, column=0)
+        .grid(row=4, column=0, pady=92)
 
-    radiobutton_spacer_2 = Label(root, pady=10, bg="#600AEF")
-    radiobutton_spacer_2.grid(row=5, column=0, columnspan=3)
+    radiobutton_spacer = Label(root, pady=10, bg="#600AEF")
+    radiobutton_spacer.grid(row=5, column=0, columnspan=3)
 
     # Second column
     Radiobutton(root, image=last_question_images[2], variable=answer, value="male", bg="#600AEF", selectcolor="#4507AB",
@@ -157,15 +166,25 @@ def last_question():
         .grid(row=4, column=1)
 
     # Third column
-    Radiobutton(root, image=last_question_images[4], variable=answer, value="female", selectcolor="#4507AB", bg="#600AEF",
-                fg="#FFFF00", activebackground="#FFFF00", activeforeground="#600AEF",
-                indicatoron=False, borderwidth=4) \
+    Radiobutton(root, image=last_question_images[4], variable=answer, value="female", selectcolor="#4507AB",
+                bg="#600AEF", fg="#FFFF00", activebackground="#FFFF00", activeforeground="#600AEF", indicatoron=False,
+                borderwidth=4) \
         .grid(row=3, column=2)
 
     Radiobutton(root, image=last_question_images[5], variable=answer, value="male", bg="#600AEF",
                 selectcolor="#4507AB", fg="#FFFF00", activebackground="#FFFF00", activeforeground="#600AEF",
                 indicatoron=False, borderwidth=4) \
         .grid(row=4, column=2)
+
+    results_button = Button(root, text="Finish quiz", bg="#600AEF", fg="#FFFF00", activebackground="#FFFF00",
+                            activeforeground="#600AEF", font=("Berlin Sans FB Demi", 20), borderwidth=6,
+                            command=results)
+    results_button.grid(row=8, column=2, sticky=SE, pady=44, padx=10)
+
+    previous_button = Button(root, text="Previous question", bg="#600AEF", fg="#FFFF00",
+                             activebackground="#FFFF00", activeforeground="#600AEF", font=("Berlin Sans FB Demi", 20),
+                             borderwidth=6, command=previous)
+    previous_button.grid(row=8, column=0, sticky=SW, pady=44, padx=10)
 
 
 # Goes back to the previous question
@@ -174,7 +193,8 @@ def previous():
     x = x - 1
     global y
     y = y - 6
-    for widget in root.winfo_children():  # Clears the main window to print the next question
+    # Clears the main window to print the next question
+    for widget in root.winfo_children():
         widget.grid_forget()
     root.grid_columnconfigure(0, weight=1)
     root.grid_columnconfigure(1, weight=1)
@@ -200,6 +220,7 @@ def previous():
                 font=("Berlin Sans FB Demi", 23), indicatoron=False, borderwidth=4) \
         .grid(row=4, column=0)
 
+    # Makes a space between the radiobuttons
     radiobutton_spacer_2 = Label(root, pady=10, bg="#600AEF")
     radiobutton_spacer_2.grid(row=5, column=0, columnspan=3)
 
@@ -257,19 +278,19 @@ def previous():
 
     next_button = Button(root, text="Next question", bg="#600AEF", fg="#FFFF00", activebackground="#FFFF00",
                          activeforeground="#600AEF", font=("Berlin Sans FB Demi", 20), borderwidth=6, command=quiz)
-    next_button.grid(row=8, column=2, sticky=SE, pady=120, padx=5)
+    next_button.grid(row=8, column=2, sticky=SE, pady=120, padx=10)
 
     if x == 1:
         previous_button = Button(root, text="Previous question", bg="#600AEF", fg="#FFFF00",
                                  activebackground="#FFFF00", activeforeground="#600AEF",
                                  font=("Berlin Sans FB Demi", 20), borderwidth=6, command=first_question)
-        previous_button.grid(row=8, column=0, sticky=SW, pady=120, padx=5)
+        previous_button.grid(row=8, column=0, sticky=SW, pady=120, padx=10)
 
     else:
         previous_button = Button(root, text="Previous question", bg="#600AEF", fg="#FFFF00",
                                  activebackground="#FFFF00", activeforeground="#600AEF",
                                  font=("Berlin Sans FB Demi", 20), borderwidth=6, command=previous)
-        previous_button.grid(row=8, column=0, sticky=SW, pady=120, padx=5)
+        previous_button.grid(row=8, column=0, sticky=SW, pady=120, padx=10)
 
 
 def quiz():     # Prints the rest of the quiz's questions
@@ -278,7 +299,8 @@ def quiz():     # Prints the rest of the quiz's questions
         x = x + 1
         global y
         y = y + 6
-        for widget in root.winfo_children():        # Clears the main window to print the next question
+        # Clears the main window to print the next question
+        for widget in root.winfo_children():
             widget.grid_forget()
         root.grid_columnconfigure(0, weight=1)
         root.grid_columnconfigure(1, weight=1)
@@ -304,6 +326,7 @@ def quiz():     # Prints the rest of the quiz's questions
                     font=("Berlin Sans FB Demi", 23), indicatoron=False, borderwidth=4)\
             .grid(row=4, column=0)
 
+        # Makes a space between the radiobuttons
         radiobutton_spacer_2 = Label(root, pady=10, bg="#600AEF")
         radiobutton_spacer_2.grid(row=5, column=0, columnspan=3)
 
@@ -316,7 +339,6 @@ def quiz():     # Prints the rest of the quiz's questions
                     fg="#FFFF00", activebackground="#FFFF00", activeforeground="#600AEF",
                     font=("Berlin Sans FB Demi", 23), indicatoron=False, borderwidth=4)\
             .grid(row=7, column=0)
-
 
         # Second column
         Radiobutton(root, image=images[y + 2], variable=answer, value="male", bg="#600AEF", selectcolor="#4507AB",
@@ -339,7 +361,6 @@ def quiz():     # Prints the rest of the quiz's questions
                     font=("Berlin Sans FB Demi", 23), indicatoron=False, borderwidth=4)\
             .grid(row=7, column=1)
 
-
         # Third column
         Radiobutton(root, image=images[y + 4], variable=answer, value="male", bg="#600AEF", selectcolor="#4507AB",
                     fg="#FFFF00", activebackground="#FFFF00", activeforeground="#600AEF",
@@ -361,24 +382,21 @@ def quiz():     # Prints the rest of the quiz's questions
                     font=("Berlin Sans FB Demi", 23), indicatoron=False, borderwidth=4)\
             .grid(row=7, column=2)
 
-
-
-
         next_button = Button(root, text="Next question", bg="#600AEF", fg="#FFFF00", activebackground="#FFFF00",
                              activeforeground="#600AEF", font=("Berlin Sans FB Demi", 20), borderwidth=6, command=quiz)
-        next_button.grid(row=8, column=2, sticky=SE, pady=120, padx=5)
+        next_button.grid(row=8, column=2, sticky=SE, pady=120, padx=10)
 
         if x == 1:
             previous_button = Button(root, text="Previous question", bg="#600AEF", fg="#FFFF00",
                                      activebackground="#FFFF00", activeforeground="#600AEF",
                                      font=("Berlin Sans FB Demi", 20), borderwidth=6, command=first_question)
-            previous_button.grid(row=8, column=0, sticky=SW, pady=120, padx=5)
+            previous_button.grid(row=8, column=0, sticky=SW, pady=120, padx=10)
 
         else:
             previous_button = Button(root, text="Previous question", bg="#600AEF", fg="#FFFF00",
                                      activebackground="#FFFF00", activeforeground="#600AEF",
                                      font=("Berlin Sans FB Demi", 20), borderwidth=6, command=previous)
-            previous_button.grid(row=8, column=0, sticky=SW, pady=120, padx=5)
+            previous_button.grid(row=8, column=0, sticky=SW, pady=120, padx=10)
 
     # Initiates the final question when it finishes with all the other questions
     except IndexError:
@@ -392,8 +410,6 @@ def first_question():
     x = 0
     global y
     y = 0
-    global z
-    z = 0
     # Clears the main window to print the next question
     for widget in root.winfo_children():
         widget.grid_forget()
@@ -429,17 +445,19 @@ def first_question():
                 indicatoron=False, borderwidth=8)\
         .grid(row=5, column=2, padx=137, ipadx=1)
 
-    next_button_spacer = Label(root, pady=153, bg="#600AEF")
+    next_button_spacer = Label(root, pady=149, bg="#600AEF")
     next_button_spacer.grid(row=6, column=2)
     next_button = Button(root, text="Next question", bg="#600AEF", fg="#FFFF00", activebackground="#FFFF00",
                          activeforeground="#600AEF", font=("Berlin Sans FB Demi", 20), borderwidth=6, command=quiz)
-    next_button.grid(row=7, column=2, padx=5, sticky=SE)
+    next_button.grid(row=7, column=2, padx=10, sticky=SE)
 
 
-def starting_interface():  # Create the starting interface
+# Creates the starting interface
+def starting_interface():
     root.grid_columnconfigure(0, weight=1)
     root.grid_columnconfigure(1, weight=1)
-    title_spacer = Label(root, bg="#600AEF")  # Pushes title text a little lower
+    # Pushes title text a little lower
+    title_spacer = Label(root, bg="#600AEF")
     title_label = Label(root, text="QUIZ GAME", bg="#600AEF", fg="#FFFF00", font=("Berlin Sans FB Demi", 65))
     description_spacer = Label(root, pady=25, bg="#600AEF")  # Pushes description label lower
     description_label = Label(root, text="Which Hollywood actor are you?", bg="#600AEF", fg="#FFFF00",
@@ -456,6 +474,7 @@ def starting_interface():  # Create the starting interface
     start_button.grid(row=5, columnspan=2)
 
 
+# Runs the starting interface
 starting_interface()
 
 root.mainloop()
